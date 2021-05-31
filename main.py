b765b15681450f6a168044a289ecae522bd8c08a
IbrahimAbdi613 <ab46467878@gmail.com>
2021-05-31 13:12:56 -0400

    Allowed front access to backend by fixing cors error

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
 import os
 import requests
 load_dotenv()
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/") 
 def home():
