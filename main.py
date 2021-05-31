c48d1cd6c46bf7eabef1df3b58dee517e318fe5f
IbrahimAbdi613 <ab46467878@gmail.com>
2021-05-30 22:20:39 -0400

    Environment variables edits

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
 import os
 import requests
+load_dotenv()
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/") 
 def home():
     return{"Hello":"World"}
 
 @app.get("/{word}")
-def getWord(word):
+def getWord(word): 
     response = requests.get(
         "https://wordsapiv1.p.rapidapi.com/words/" + word, 
-        headers={"X-Mashape-Key": os.getenv('WORDS_API_KEY')}
+        headers={"X-Mashape-Key": os.environ.get('WORDS_API_KEY')}
     )
+    print(os.environ.get('WORDS_API_KEY'))
     return {"Data":response.json()}
 
     
